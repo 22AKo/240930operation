0751542796938f3e9ad5c95aba8e42d7465a2c36
Alfa Ko <22AKo@heckgrammar.co.uk>
2024-09-30 11:18:33 +0100

    Add project files.

---
+++ 240930operation/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _240930operation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tempNum = 0;
+            Console.WriteLine("enter first number");
+            double numOne = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            double numTwo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter first int");
+            int intOne = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("enter second int");
+            int intTwo = Convert.ToInt32(Console.ReadLine());
+            tempNum = intOne + intTwo;
+            Console.WriteLine("int 1 + int 2 = " + tempNum);
+            tempNum = numOne + numTwo;
+            Console.WriteLine("num 1 + num 2 = " + tempNum);
+            tempNum = numOne + intOne;
+            Console.WriteLine("num 1 + int 1 = " + tempNum);
+            tempNum = intOne - intTwo;
+            Console.WriteLine("int 1 - int 2 = " + tempNum);
+            tempNum = numOne - numTwo;
+            Console.WriteLine("num 1 - num 2 = " + tempNum);
+            tempNum = numOne - intOne;
+            Console.WriteLine("num 1 - int 1 = " + tempNum);
+            tempNum = intOne * intTwo;
+            Console.WriteLine("int 1 * int 2 = " + tempNum);
+            tempNum = numOne * numTwo;
+            Console.WriteLine("num 1 * num 2 = " + tempNum);
+            tempNum = numOne * intOne;
+            Console.WriteLine("num 1 * int 1 = " + tempNum);
+            tempNum = 7/2
+            Console.
+        }   
+    }
+}
