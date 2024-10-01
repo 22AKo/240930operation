1fe260bd72059d2f8dab9991a5d0dec579eb7aa8
Alfa Ko <22AKo@heckgrammar.co.uk>
2024-10-01 15:15:06 +0100

    funnnnn

--- 240930operation/Program.cs
+++ 240930operation/Program.cs
@@ -31,6 +31,37 @@
             Console.WriteLine("num 1 * num 2 = " + tempNum);
             tempNum = numOne * intOne;
             Console.WriteLine("num 1 * int 1 = " + tempNum);
+            tempNum = intOne / intTwo;
+            Console.WriteLine("int 1 / int 2 = " + tempNum);
+            tempNum = numOne / numTwo;
+            Console.WriteLine("num 1 / num 2 = " + tempNum);
+            tempNum = numOne / intOne;
+            Console.WriteLine("num 1 / int 1 = " + tempNum);
+            tempNum = intOne % intTwo;
+            Console.WriteLine("int 1 mod int 2 = " + tempNum);
+            tempNum = numOne % numTwo;
+            Console.WriteLine("num 1 mod num 2 = " + tempNum);
+            tempNum = numOne % intOne;
+            Console.WriteLine("num 1 mod int 1 = " + tempNum);
+            
+            if (numOne == numTwo)
+            {
+                Console.WriteLine("num 1 is equal to num 2");
+            };
+            if (intOne != intTwo)
+            {
+                Console.WriteLine("int 1 isnt equal to int 2")
+            };
+            if (numOne <= intOne)
+            {
+                Console.WriteLine("num one is less tan or equal to int one")
+            };
+            if (numOne >= intOne)
+            {
+                Console.WriteLine("num two is more tan or equal to int two")
+            };
+            Console.WriteLine("enter to end program");
+            string endString = Console.ReadLine();
         }   
     }
 }
