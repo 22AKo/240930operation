155b76ceeac9116f61635bc5aba0cca40c2ae6a9
Alfa Ko <22AKo@heckgrammar.co.uk>
2024-10-07 11:20:39 +0100

    eppy time

--- 240930operation/Program.cs
+++ 240930operation/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            double tempInt = 0;
             double tempNum = 0;
             Console.WriteLine("enter first number");
             double numOne = Convert.ToDouble(Console.ReadLine());
@@ -50,17 +51,73 @@
             };
             if (intOne != intTwo)
             {
-                Console.WriteLine("int 1 isnt equal to int 2")
+                Console.WriteLine("int 1 isnt equal to int 2");
             };
             if (numOne <= intOne)
             {
-                Console.WriteLine("num one is less tan or equal to int one")
+                Console.WriteLine("num one is less tan or equal to int one");
             };
             if (numOne >= intOne)
             {
-                Console.WriteLine("num two is more tan or equal to int two")
+                Console.WriteLine("num two is more tan or equal to int two");
             };
-            Console.WriteLine("enter to end program");
+            if (numOne > intOne)
+            {
+                Console.WriteLine("num one is more than int one");
+            };
+            if (numOne < intOne)
+            {
+                Console.WriteLine("num two is less tan int two");
+            };
+            bool bolOne = int.TryParse(Convert.ToString(numOne), out _);
+            bool bolTwo = int.TryParse(Convert.ToString(numTwo), out _);
+            if (bolOne)
+            {
+                Console.WriteLine("num 1 is an int");
+            };
+            if (!bolTwo)
+            {
+                Console.WriteLine("num 2 is not an int");
+            };
+            if (bolOne && bolTwo)
+            {
+                Console.WriteLine("both num 1 and 2 are ints");
+            };
+            if (bolOne || bolTwo)
+            {
+                Console.WriteLine("either num 1 or 2 are ints");
+            };
+            if (bolOne ^ bolTwo)
+            {
+                Console.WriteLine("only one of num 1 or 2 are ints");
+            };
+            if (intOne > intTwo)
+            {
+                Console.WriteLine("values between int 1 and 2 include:");
+                for (int i = intTwo; i < intOne; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else if (intOne < intTwo)
+            {
+                Console.WriteLine("values between int 1 and 2 consist of:");
+                for (int i = intTwo; i > intOne; i--)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("values between int 1 and 2 consist of:");
+                Console.WriteLine(intOne);
+            }
+            tempNum = intOne % intTwo;
+            for (int i = 0; i < tempNum; i++)
+            {
+                Console.WriteLine(i + ". enter to end program");
+            }
+           
             string endString = Console.ReadLine();
         }   
     }
